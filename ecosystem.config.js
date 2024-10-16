91f6df6dd8e622b85c88ab26e9f4438d7554191f
Nils Flaschel <n.flaschel@gmail.com>
2024-10-16 19:00:56 +0200

    eco fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,10 @@ module.exports = {
     apps: [
       {
         name: 'DashAutumnApp',
-        script: '/root/miniconda3/envs/dash-autumn-app/bin/gunicorn -w 2 -b 0.0.0.0:8090 app:server',
+        script: '/root/miniconda3/envs/dash-autumn-app/bin/gunicorn -w 4 -b 0.0.0.0:8090 app:server',
         instances: 1,
         autorestart: true,
         watch: true,
-        max_memory_restart: '4G',
         env: {
           NODE_ENV: 'production',
         },
